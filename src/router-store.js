4567239db3e35775292fb80fca8ba8652f0b287b
Tim Kendall <tim@acorns.com>
2017-01-22 19:19:37 -0800

    Make goTo action async

--- src/router-store.js
+++ src/router-store.js
@@ -1,4 +1,4 @@
-import {observable, computed, action, toJS} from 'mobx';
+import {observable, computed, action, runInAction, toJS} from 'mobx';
 
 class RouterStore {
 
@@ -10,7 +10,7 @@ class RouterStore {
     this.goTo = this.goTo.bind(this);
   }
 
-  @action goTo(view, paramsObj, store, queryParamsObj) {
+  @action async goTo(view, paramsObj, store, queryParamsObj) {
 
     const nextPath = view.replaceUrlParams(paramsObj, queryParamsObj);
     const pathChanged = nextPath !== this.currentPath;
@@ -23,21 +23,33 @@ class RouterStore {
     const currentParams = toJS(this.params);
     const currentQueryParams = toJS(this.queryParams);
 
-    const beforeExitResult = (rootViewChanged && this.currentView && this.currentView.beforeExit) ? this.currentView.beforeExit(this.currentView, currentParams, store, currentQueryParams) : true;
-    if (beforeExitResult === false) {
-      return;
+    // Run `beforeExit` hook if it exists on the current view
+    if (rootViewChanged && this.currentView && this.currentView.beforeExit) {
+      const beforeExitResult = await this.currentView.beforeExit(this.currentView, currentParams, store, currentQueryParams);
+      if (beforeExitResult === false) {
+        return;
+      }
     }
+   
+    // Run `beforeEnter` hook if it exists on the current route
+    if (rootViewChanged && view.beforeEnter) {
+        const beforeEnterResult = await view.beforeEnter(view, currentParams, store, currentQueryParams);
+        if (beforeEnterResult === false) {
+          return;
+        }
 
-    const beforeEnterResult = (rootViewChanged && view.beforeEnter) ? view.beforeEnter(view, currentParams, store, currentQueryParams) : true
-    if (beforeEnterResult === false) {
-      return;
     }
-
+   
+    // Run `onExit` hook if it exists on the current route
     rootViewChanged && this.currentView && this.currentView.onExit && this.currentView.onExit(this.currentView, currentParams, store, currentQueryParams);
 
-    this.currentView = view;
-    this.params = toJS(paramsObj);
-    this.queryParams = toJS(queryParamsObj);
+    // MobX requires us to update the state in `runAction` as this @action is now async (see https://mobx.js.org/refguide/action.html)
+    runInAction('update router state after async beforeExit and beforeEnter', () => {
+      this.currentView = view;
+      this.params = toJS(paramsObj);
+      this.queryParams = toJS(queryParamsObj);
+    });
+    
     const nextParams = toJS(paramsObj);
     const nextQueryParams = toJS(queryParamsObj);
 
