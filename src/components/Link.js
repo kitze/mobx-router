4c4fd1f9a45d7e52226728940074e4e3956c579a
Naresh Bhatia <naresh.archfirst@gmail.com>
2017-12-15 14:48:36 -0500

    Added back react imports in Link and MobxRouter

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line
+import React from 'react';
 import { observer } from 'mobx-react';
 
 const Link = ({
--- src/components/MobxRouter.js
+++ src/components/MobxRouter.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line
+import React from 'react';
 import { observer, inject } from 'mobx-react';
 
 const MobxRouter = ({ store: { router } }) => (
