77dcf8f572d802c877faaa2328e380217b738527
thdk <t.dekiere@gmail.com>
2018-12-05 15:26:48 +0100

    Removed the wrapping div (fix second typo)
    Fixes kitze/mobx-router#30

--- src/components/MobxRouter.js
+++ src/components/MobxRouter.js
@@ -2,12 +2,11 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
 
-const MobxRouterBase = ({ store: { router } }) => {
+const MobxRouterBase = ({ store: { router } }) =>
     router.currentView && router.currentView.component ? (
         router.currentView.component
     ) : (
         <div />
     );
-};
 
 export const MobxRouter = inject('store')(observer(MobxRouterBase));
