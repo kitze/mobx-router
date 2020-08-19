33d9f136ef70855089ce26c2c001fd7b27504633
Takanori Sato <takanori.sato.xh@hitachi.com>
2020-08-19 17:23:16 +0900

    add test for hash routing

---
+++ tests/hash-routing.spec.ts
@@ -0,0 +1,22 @@
+import { routes } from './mocks/routes';
+import { RootStore } from './mocks/store';
+import { startRouter } from '../src';
+
+const rootStore = new RootStore();
+
+test('Hash routing Scenario', () => {
+    startRouter(routes, rootStore, {
+        html5history: false
+    })
+    const { router } = rootStore;
+    router.currentRoute = routes.home;
+
+    expect(router.currentPath).toBe('/');
+    expect(window.location.pathname).toBe('/');
+    expect(window.location.hash).toBe('#/');
+
+    router.goTo(routes.profile, { username: 'kitze' }, { id: "123" });
+    expect(router.currentPath).toBe('/profile/kitze?id=123');
+    expect(window.location.pathname).toBe('/');
+    expect(window.location.hash).toBe('#/profile/kitze?id=123');
+});
