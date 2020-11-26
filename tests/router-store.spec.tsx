6105d52da8a9720136b6fe4027c132e1499d69e2
thdk <t.dekiere@gmail.com>
2020-11-26 18:29:58 +0100

    Add router store spec

---
+++ tests/router-store.spec.tsx
@@ -0,0 +1,59 @@
+// eslint-disable-next-line
+import React from 'react';
+
+import { autorun, reaction, toJS } from "mobx";
+
+import { Route, RouterStore, startRouter } from "../src";
+
+const path = '/profile/:username/:tab';
+const routes = {
+    home: new Route<any>({
+        path: "/",
+        component: <>HOME</>,
+    }),
+    profile: new Route<any>({
+        path,
+        component: <div />
+    }),
+};
+
+describe("RouterStore", () => {
+    test("it should trigger autorun when currentRoute is changed", () => {
+        const router = new RouterStore({} as any);
+        const autorunCallback = jest.fn();
+        autorun(() => {
+            router.currentRoute;
+            autorunCallback();
+        });
+
+        router.goTo(routes.profile);
+
+
+        expect(autorunCallback).toHaveBeenCalledTimes(2);
+    });
+
+    test("if currentRoute is observable", () => {
+        const store = {
+            router: new RouterStore({} as any),
+        };
+
+        startRouter(routes, store);
+        const reactionCallback = jest.fn();
+        const dispose = reaction(() => store.router.currentRoute, route => reactionCallback(toJS(route)));
+        store.router.goTo(routes.profile);
+
+        expect(reactionCallback).toHaveBeenCalledWith(
+            expect.objectContaining({
+                path: path,
+            }),
+        );
+
+        store.router.goTo(routes.home);
+
+        store.router.goTo(routes.profile);
+
+        expect(reactionCallback).toHaveBeenCalledTimes(3);
+
+        dispose();
+    });
+});
